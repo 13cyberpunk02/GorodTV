1b96adc7dfa42a4d9bf4822306674f98647e6088
cyber <salawat1302@gmail.com>
2024-12-17 23:00:57 +0300

    Project created and ready for start.

---
+++ GorodTV/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace GorodTV;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .UseMauiCommunityToolkitMediaElement()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Pacifico.ttf", "Pacifico");
+                fonts.AddFont("RobotoSlab.ttf", "RobotoSlab");
+                fonts.AddFont("RussoOne-Regular.ttf", "RussoOneRegular");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
