dbad802e9cf31ee8678fb8a3e8e2266f02f9175d
cyber <salawat1302@gmail.com>
2024-12-18 02:26:16 +0300

    Category View Model done.

---
+++ GorodTV/ModelViews/CategoryViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GorodTV.Models.Responses.Category;
+using GorodTV.Pages;
+using GorodTV.Services;
+using GorodTV.Services.Interfaces;
+
+namespace GorodTV.ModelViews;
+
+public partial class CategoryViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<Category> _categories;
+    
+    [ObservableProperty]
+    private Category _selectedCategory;
+    
+    private IRestService _restService;
+    public IAsyncRelayCommand LoadCategoriesCommand { get; }
+
+    public CategoryViewModel()
+    {
+        Categories = new ObservableCollection<Category>();
+        LoadCategoriesCommand = new AsyncRelayCommand(LoadCategoriesAsync);
+        _restService = new RestService();
+    }
+
+    private async Task LoadCategoriesAsync()
+    {
+        var categories = await _restService.GetCategoriesRequest();
+
+        if (!categories.Categories.Any())
+        {
+            await Shell.Current.DisplayAlert("Ошибка", "Похоже, что вы не авторизованы", "OK");
+            return;
+        }
+        Categories.Clear();
+        foreach (var category in categories.Categories)
+        {
+            Categories.Add(category);
+        }
+    }
+
+    [RelayCommand]
+    private async Task SelectCategoryAsync()
+    {
+        if(SelectedCategory is null)
+            return;
+
+        var parameters = new Dictionary<string, object>
+        {
+            { "categoryId", SelectedCategory.Id },
+            { "categoryName", SelectedCategory.Name }
+        };
+        SelectedCategory = null;
+        await Shell.Current.GoToAsync($"///{nameof(ChannelPage)}", parameters);
+    }
+
+    partial void OnSelectedCategoryChanged(Category value)
+    {
+        if (value is not null)
+        {
+            SelectCategoryCommand.Execute(null);
+        }
+    }
+}
